04b0ceb0ba272c0f1198960ba62be20097fa78fc
Katrine-Marie <fjordscene@gmail.com>
2018-11-06 16:51:16 +0100

    Created class and adjusted the procedural code to fit

--- index.php
+++ index.php
@@ -2,26 +2,32 @@
 /*
 Plugin Name: Nebula Custom Post Type
 Description: Plugin for creating a custom post type (EVENT)
-Version: 0.1.0
+Version: 1.0.0
 Author: Katrine-Marie Burmeister
 */
 
 //Custom post types
-add_action('init', 'create_post_type');
-function create_post_type(){
-  register_post_type('event', array('labels' => array(
-      'name' => __('Events'),
-      'singular_name' => __('Event'),
-      'add_new' => __('Add new'),
-      'add_new_item' => __('Add new event')
-    ),
-    'public' => true,
-    'has_archive' => true,
-    'show_in_nav_menus' => true,
-    'menu_icon' => 'dashicons-calendar-alt',
-    'taxonomies' => array('category', 'post_tag')
-    )
-  );
+class EventClass {
+
+  function __construct(){
+    add_action('init', 'create_post_type');
+  }
+
+  function create_post_type(){
+    register_post_type('event', array('labels' => array(
+        'name' => __('Events'),
+        'singular_name' => __('Event'),
+        'add_new' => __('Add new'),
+        'add_new_item' => __('Add new event')
+      ),
+      'public' => true,
+      'has_archive' => true,
+      'show_in_nav_menus' => true,
+      'menu_icon' => 'dashicons-calendar-alt',
+      'taxonomies' => array('category', 'post_tag')
+      )
+    );
+  }
 }
 
 ?>
