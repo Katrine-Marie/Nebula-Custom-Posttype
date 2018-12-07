ac46b21203da803e6fc2e937d0565a7b321fd85c
Katrine-Marie <fjordscene@gmail.com>
2018-12-07 13:57:33 +0100

    Adjusted posttype class to use options instead of hardcoded values

--- index.php
+++ index.php
@@ -1,17 +1,21 @@
 <?php
 /*
 Plugin Name: Nebula Custom Post Type
-Description: Plugin for creating a custom post type (EVENT)
+Description: Plugin for creating a custom post type
 Version: 1.0.0
 Author: Katrine-Marie Burmeister
 Author URI: https://fjordstudio.dk
 */
 
+require_once(__DIR__.'/includes/admin-page.php');
+
 class CustomPosttype {
   private $labels;
+  private $icon;
 
-  function __construct($labels){
+  function __construct($labels, $icon){
     $this->labels = $labels;
+    $this->icon = $icon;
     add_action('init', array($this,'create_post_type'));
   }
 
@@ -21,7 +25,7 @@ class CustomPosttype {
       'public' => true,
       'has_archive' => true,
       'show_in_nav_menus' => true,
-      'menu_icon' => 'dashicons-calendar-alt',
+      'menu_icon' => $this->icon,
       'taxonomies' => array('category', 'post_tag')
       )
     );
@@ -30,18 +34,20 @@ class CustomPosttype {
 }
 
 $labels = array(
-	'name' => __('Events'),
-	'singular_name' => __('Event'),
+	'name' => get_option('myplugin_option_name'),
+	'singular_name' => get_option('myplugin_option_name'),
 	'add_new' => __('Add New'),
-	'add_new_item' => __('Add New Event'),
-	'edit_item' => __('Edit Event'),
-	'new_item' => __('New Event'),
-	'view_item' => __('View Event'),
-	'view_items' => __('View Events'),
-	'search_items' => __('Search Events'),
-	'all_items' => __('All Events')
+	'add_new_item' => 'Add New '.get_option('myplugin_option_name'),
+	'edit_item' => 'Edit '.get_option('myplugin_option_name'),
+	'new_item' => 'New '.get_option('myplugin_option_name'),
+	'view_item' => 'View '.get_option('myplugin_option_name'),
+	'view_items' => 'View '.get_option('myplugin_option_name_plural'),
+	'search_items' => 'Search '.get_option('myplugin_option_name_plural'),
+	'all_items' => 'All '.get_option('myplugin_option_name_plural')
 );
 
-$event = new CustomPosttype();
+$icon = get_option('myplugin_option_icon');
+
+$posttype = new CustomPosttype($labels, $icon);
 
 ?>
