dce3b5d44639ad2c13b83674d8f51e8ded27251e
Katrine-Marie Burmeister <fjordscene@gmail.com>
2019-02-14 10:03:44 +0100

    Update license + license URI

--- nebula-custom-posttype.php
+++ nebula-custom-posttype.php
@@ -8,8 +8,8 @@
 * Author: Katrine-Marie Burmeister
 * Author URI: https://fjordstudio.dk
 * Text Domain: nebula-pt
-* License:     GPL-2.0+
-* License URI: http://www.gnu.org/licenses/gpl-2.0.txt
+* License:     GNU General Public License v3.0
+* License URI: https://www.gnu.org/licenses/gpl-3.0.html
 *
 **/
 
